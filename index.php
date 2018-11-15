ee2e8cb3cc3645ed1d483123d7baca2209328aa1
Jonathan Mackenzie <jonmac1@gmail.com>
2018-11-15 12:44:13 +1030

    Create index.php

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Jonathan Mackenzie</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" />
+
+    <!-- Custom CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:400,300|Lora:700" rel="stylesheet">
+   
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                  <span class="light">Jonathan</span> Mackenzie
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li><a href="https://blog.jonno.top">Blog</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Me</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#download">Projects</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">Jonathan Mackenzie</h1>
+                        <p class="intro-text">Phd Student, Flinders University<br>Android. Python. Fencing.</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-left">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h1>About Me</h1>
+		<p>I am a PhD student at Flinders University. My research focuses on predicting and detecting incidents in short term traffic flow.<br>
+		My other interests include self-teaching and learning about: 
+		<ul class="list">
+		    <li>Algorithmic art</li>
+		    <li>Machine learning</li>
+		    <li>Parallel programming with OpenCL</li>
+		    <li>Web development with a Python/Linux/nginx/bootstrap workflow</li>
+		    <li>Veganism</li>
+		    <li>Coaching fencing</li>
+		</ul>
+		</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Download Section -->
+    <section id="download" class="content-section text-left">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Projects</h2>
+			<p>Most of my projects, including my PhD work and publications, are available open source on <a href="https://github.com/jonnoftw/">github profile</a>
+			<ul class="list">
+			    <li><a href="https://github.com/JonnoFTW/rpi-can-logger">Rpi-can-logger</a> project: software package for raspberry pi to log, interpret and upload CAN data from a variety of vehicle types</li>
+			    <li><a href="https://github.com/JonnoFTW/webcan">Webcan</a>: a web based viewer and analysis suite for use with rpi-can-logger</li>
+			    <li><a href="https://github.com/JonnoFTW/nn-cl">nn-cl</a>: an in-development neural network library written in opencl/pyopencl/python</li>
+			    <li><a href="https://github.com/JonnoFTW/htm-models-adelaide">HTM-Models-Adelaide</a>: project for predicting traffic flow and visualising results </li>
+			    <li><a href="https://github.com/JonnoFTW/img_term">img_term</a>: a fun library to render images/video/usb camera to your terminal</li>
+			    <li><a href="https://github.com/JonnoFTW/pyramid_mongodb2">pyramid_mongodb2</a>: plugin for pyramid to better integrate mongodb into your pyramid project, including a comprehensive debugtoolbar addon</li>
+			    <li><a href="https://github.com/JonnoFTW/traffic-prediction">traffic-prediction</a>: using LSTM with python/keras  to predict traffic flow</li>
+			    <li><a href="https://github.com/JonnoFTW/TonsleyLEDManager">TonsleyLEDManager</a>: pyramid/mysql/pygame project to display animations on the LED display at Flinders Tonsley</li>
+			    <li><a href="https://github.com/JonnoFTW/BibTeX-Label-Creator">Bibtex-Label-Creator</a>: generate Bibtex labels for Endnote</li>
+			    <li><a href="https://github.com/JonnoFTW/markov-img-gen">Markov-img-gen</a>: Experiment in learning/generating images as a Markov process using maximum likelihood</li>
+			    <li><a href="https://github.com/JonnoFTW/album2mp4">Album2mp4</a>: script using python/ffmpeg to generate videos from albums</li>
+			    <li><a href="https://github.com/JonnoFTW/TwatBot2">Twatbot2</a>: IRC bot with a lot of features </li>
+			    <li><a href="https://github.com/JonnoFTW/Android-Oscilloscope-View">Android Oscilloscope view</a>: a custom view for Android that renders a view as an oscilloscope would with an erasor</li>
+			    <li><a href="https://github.com/JonnoFTW/htm-over-http">htm-over-http</a>: a small library to run and interact with HTM models via REST interface </li>
+			</ul>
+			<hr/>
+			<h2>Open Source Contributions</h2>
+			<p>I've contributed to a number of open source projects, including:</p>
+			
+			<ul class="list">
+			    <li><a href="https://github.com/numenta/nupic">Nupic</a>: a biologically inspired framework for prediction and anomaly detection</li>
+			    <li><a href="https://github.com/maxpumperla/hyperas">Hyperas</a>: a template based library for hyperparameter optimisation</li>
+			    
+			</ul>
+			<hr/>
+			<h2>Android Apps</h2>
+			<p>Released a number of apps on the google play store for Android:</p>
+			<ul class="list">
+                    <li><a href="https://play.google.com/store/apps/details?id=com.jonathanmackenzie.scrabblehelper">Scrabble Helper</a></li>
+                    <li><a href="https://play.google.com/store/apps/details?id=com.jonathanmackenzie.sms_morse">SMS Morse</a></li>
+                    <li><a href="https://play.google.com/store/apps/details?id=au.com.virtueoftheday.virtuedaily">Virtue of the Day</a></li>
+                    <li><a href="https://play.google.com/store/apps/details?id=jonathanmackenzie.hydilogger">HYDI Monitor</a></li>
+                    <li><a href="https://play.google.com/store/apps/details?id=jonathanmackenzie.com.locationcamera">QR 4 Kids</a></li>
+			</ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="publications" class="content-section text-left"> 
+        
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Publications</h2>
+                <h4>Traffic Prediction using HTM and LSTM</h4>
+                <p><a href="https://ieeexplore.ieee.org/document/8424074">An Evaluation of HTM and LSTM for Short-Term Arterial Traffic Flow Prediction</a>: Published in IEEE Transactions on Intelligent Transportation Systems, investigating
+                and comparing HTM and LSTM for short term arterial traffic prediction</p>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center mt-5">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact</h2>
+                <p>Feel free to contact me through the following means:</p>
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://au.linkedin.com/in/mackenziejonathan" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">LinkedIn</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/jonnoftw" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="https://jonnoftw.github.io" class="btn btn-default btn-lg"><i class="fa fa-th fa-fw"></i> <span class="network-name">Blog</span></a>
+                    </li>
+			<li><a href="irc://irc.freenode.net/jonno_ftw" class="btn btn-default btn-lg"><i class="fa fa-slack fa-fw"></i> <span class="network-name">IRC</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; Jonathan Mackenzie  <? echo date('Y');?></p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
+
+</body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42368583-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</html>
